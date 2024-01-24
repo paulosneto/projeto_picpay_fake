5aec52c8d2e516f9fd8ae3b1ef84912c2241559a
paulo_sneto <paulo_sneto@yahoo.com>
2024-01-24 11:27:12 -0300

    feat: create UserRepository

---
+++ src/main/java/br/com/projetopicpay/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.projetopicpay.repositories;
+
+import br.com.projetopicpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+
+}
