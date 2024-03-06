a2c6179ebe0fd76266a897e4062724c8c6c4309d
paulo_sneto <paulo_sneto@yahoo.com>
2024-03-06 11:02:36 -0300

    feat: create TransactionService.java

--- src/main/java/br/com/projetopicpay/services/TransactionService.java
+++ src/main/java/br/com/projetopicpay/services/TransactionService.java
@@ -28,7 +28,10 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void createTransaction(TransactionDTO transactionDTO) throws Exception {
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transactionDTO) throws Exception {
 
         User sender = this.userService.findUserById(transactionDTO.senderId());
         User receiver = this.userService.findUserById(transactionDTO.receiverId());
@@ -53,9 +56,17 @@ public class TransactionService {
         // Faz a adição no valor de recebimento da transação
         receiver.setBalance(receiver.getBalance().add(transactionDTO.value()));
 
-        repository.save(newTransaction);
+        this.repository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transação realizada com sucesso");
+        this.notificationService.sendNotification(receiver, "Transação recebida com sucesso");
+
+        return newTransaction;
+
     }
-    //ab1504!@#
+
     public boolean authorizeTransaction(User sender, BigDecimal value){
         //ResponseEntity<Map> autResponseEntity = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6",Map.class);
         ResponseEntity<Map> autResponseEntity = restTemplate.getForEntity("https://run.mocky.io/v3/60a52d40-4bab-45fc-ac73-128b7c42aa2b",Map.class);
