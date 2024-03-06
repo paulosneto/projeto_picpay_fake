e9c5f9b47d1a79ef15e975a5bb6ead26e7df192c
paulo_sneto <paulo_sneto@yahoo.com>
2024-03-06 10:59:30 -0300

    fix: update Transaction.java

--- src/main/java/br/com/projetopicpay/domain/transaction/Transaction.java
+++ src/main/java/br/com/projetopicpay/domain/transaction/Transaction.java
@@ -2,18 +2,19 @@ package br.com.projetopicpay.domain.transaction;
 
 import br.com.projetopicpay.domain.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity(name = "transactions")
 @Table(name = "transactions")
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Transaction {
 
@@ -24,11 +25,12 @@ public class Transaction {
     private BigDecimal amaount;
 
     @ManyToOne // Esta anotação quer dizer que o usuario pode ter varios envios, mas o contrario nao é verdadeiro
-    @JoinColumn(name = "receiver_id")
+    @JoinColumn(name = "sender_id")
     private User sender;
 
     @ManyToOne
-    @JoinColumn(name = "receiver_id ")
+    @JoinColumn(name = "receiver_id")
     private User receiver;
 
+    private LocalDateTime timestamp;
 }
